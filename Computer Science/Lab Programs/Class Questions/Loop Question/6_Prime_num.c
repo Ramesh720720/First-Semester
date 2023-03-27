466c6f156335ee16639b845522b906e8af8791b9
Ramesh720720 <114826244+Ramesh720720@users.noreply.github.com>
2023-03-27 16:21:15 +0530

    format

--- Computer Science/Lab Programs/Class Questions/Loop Question/6_Prime_num.c
+++ Computer Science/Lab Programs/Class Questions/Loop Question/6_Prime_num.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 int main()
 {
-   int num;
-   printf("Enter a number : ");
-   scanf("%d",&num);
-    int boolean=0;
-    int result =0;
-   for (int i = 2; i < num; i++)
-   {    
-    if(boolean==0){
-        if(num%i==0){
-            result=1;
-            boolean=1;
+    int num;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    int boolean = 0;
+    int result = 0;
+    for (int i = 2; i < num; i++)
+    {
+        if (boolean == 0)
+        {
+            if (num % i == 0)
+            {
+                result = 1;
+                boolean = 1;
+            }
         }
     }
-   }
-   if(result==1){
-    printf("Not A Prime Number \n");
-   }else{
-    printf("Prime Number \n");
-   }
+    if (result == 1)
+    {
+        printf("Not A Prime Number \n");
+    }
+    else
+    {
+        printf("Prime Number \n");
+    }
     return 0;
 }
