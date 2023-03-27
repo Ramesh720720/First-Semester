fe6eee269b5b42749c753c159f557f4de73245a2
Ramesh720720 <114826244+Ramesh720720@users.noreply.github.com>
2023-03-27 16:27:03 +0530

    calci function

---
+++ Computer Science/Lab Programs/Class Questions/Function/calci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int sum(int, int);
+int sub(int, int);
+int multi(int, int);
+float divide(int, int);
+int main()
+{
+    return 0;
+}
+int sum(int x,int y){
+    return x+y;
+}
+ int sub(int x,int y){
+    return x-y;
+}
+int  multi(int x,int y){
+    return x*y;
+}
+float divide(int x,int y){
+    return x/y;
+}
