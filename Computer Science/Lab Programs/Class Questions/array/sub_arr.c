57d2e406010952498c0d396bb1a32ecfa66803e2
Ramesh720720 <114826244+Ramesh720720@users.noreply.github.com>
2023-03-28 18:12:44 +0530

    update

--- Computer Science/Lab Programs/Class Questions/array/sub_arr.c
+++ Computer Science/Lab Programs/Class Questions/array/sub_arr.c
@@ -1,45 +1,46 @@
-#include<stdio.h>
-int main(){
-    // subtraction of 2 matrics 
-    int row,column,i,j;
+#include <stdio.h>
+int main()
+{
+    // subtraction of 2 matrics
+    int row, column, i, j;
     printf("Enter row of matrices : ");
-    scanf("%d",&row);
+    scanf("%d", &row);
     printf("Enter column of matrices : ");
-    scanf("%d",&column);
-    int matric1[row][column],matric2[row][column],result[row][column];
+    scanf("%d", &column);
+    int matric1[row][column], matric2[row][column], result[row][column];
+    printf("\nEnter matrics 1 : \n");
     for (i = 0; i < row; i++)
     {
-        for ( j = 0; j < column; j++)
+        for (j = 0; j < column; j++)
         {
-            printf("Enter value on row %d and column %d : ",i,j);
-            scanf("%d",&matric1[i][j]);
+            printf("Enter value on row %d and column %d : ", i, j);
+            scanf("%d", &matric1[i][j]);
         }
-        
     }
-     for (i = 0; i < row; i++)
+    printf("\nEnter matrics 2 : \n");
+    for (i = 0; i < row; i++)
     {
-        for ( j = 0; j < column; j++)
+        for (j = 0; j < column; j++)
         {
-            printf("Enter value on row %d and column %d : ",i,j);
-            scanf("%d",&matric2[i][j]);
+            printf("Enter value on row %d and column %d : ", i, j);
+            scanf("%d", &matric2[i][j]);
         }
-        
     }
-    int sub=0;
-     for (i = 0; i < row; i++)
+    int sub = 0;
+    for (i = 0; i < row; i++)
     {
-        for ( j = 0; j < column; j++)
+        for (j = 0; j < column; j++)
         {
-            sub=matric1[i][j]-matric2[i][j];
-            result[i][j]=sub;
+            sub = matric1[i][j] - matric2[i][j];
+            result[i][j] = sub;
         }
     }
     printf("\nResult is :\n");
-     for (i = 0; i < row; i++)
+    for (i = 0; i < row; i++)
     {
-        for ( j = 0; j < column; j++)
+        for (j = 0; j < column; j++)
         {
-            printf("%d\t",result[i][j]);
+            printf("%d\t", result[i][j]);
         }
         printf("\n");
     }
