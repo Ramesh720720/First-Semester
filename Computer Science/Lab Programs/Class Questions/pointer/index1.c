c1028a050c2142155e7223b17b589084538391ea
Ramesh720720 <114826244+Ramesh720720@users.noreply.github.com>
2023-03-29 13:35:38 +0530

    pointer

--- Computer Science/Lab Programs/Class Questions/pointer/index1.c
+++ Computer Science/Lab Programs/Class Questions/pointer/index1.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int main(){
-    
+    int *p;
+    char *c;
+    c="00661FF18";
+    p= *c;
+    printf("%d\n",*p);
     return 0;
 }
--- Computer Science/Lab Programs/Class Questions/pointer/index2.c
+++ Computer Science/Lab Programs/Class Questions/pointer/index2.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 int main(){
-    
+    int x=20;
+    int *p;
+    p=&x;
+    printf("%d\n",x);
+    printf("%p\n",&x);
+    printf("%p\n",&p);
+    printf("%d\n",*p);
     return 0;
 }
